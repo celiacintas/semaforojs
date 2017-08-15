5ddedd26de681cfe7bc3deb289c0203db08267b7
Celia Cintas <cintas.celia@gmail.com>
2017-08-15 16:39:34 -0300

    remove old sleep func

--- static/js/app.js
+++ static/js/app.js
@@ -188,15 +188,6 @@ $(function() {
         clock.start()
     }
 
-    function sleep(milliseconds) {
-        var start = new Date().getTime();
-        for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds) {
-                break;
-            }
-        }
-    }
-
     init();
     animate();
 });
